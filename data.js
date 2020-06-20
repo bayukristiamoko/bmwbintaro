b35974061d4223766cca7f2942871c45c73c66db
bayukristiamoko <bayukristiamoko@gmail.com>
2020-06-20 22:30:11 +0700

    bmwcilandak

---
+++ data.js
@@ -0,0 +1,493 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-information",
+      "name": "Reception Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.09725553654763175,
+        "pitch": 0.10928511644517158,
+        "fov": 1.3491744678877828
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.715009444517376,
+          "pitch": 0.2580387937647721,
+          "rotation": 0,
+          "target": "3-drive-in-gallery"
+        },
+        {
+          "yaw": -1.6430693772582146,
+          "pitch": 0.23332446311367327,
+          "rotation": 0,
+          "target": "2-drive-in-gallery-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-diagnose-room",
+      "name": "Diagnose Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.568049228981586,
+          "pitch": 0.16482549746723762,
+          "rotation": 0,
+          "target": "9-service-room"
+        },
+        {
+          "yaw": -3.110962819495036,
+          "pitch": 0.18352554130919785,
+          "rotation": 0,
+          "target": "8-reception-at-the-car"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-drive-in-gallery-2",
+      "name": "Driving Gallery",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9024348771643638,
+          "pitch": 0.1928141605112419,
+          "rotation": 0,
+          "target": "0-information"
+        },
+        {
+          "yaw": 3.0159672919041327,
+          "pitch": 0.1776149484512075,
+          "rotation": 0,
+          "target": "5-lifestyle--sa"
+        },
+        {
+          "yaw": 1.9437800874275055,
+          "pitch": 0.04881600586166179,
+          "rotation": 0,
+          "target": "8-reception-at-the-car"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-drive-in-gallery",
+      "name": "Driving Gallery 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.6219319230853806,
+        "pitch": -0.002483752646480042,
+        "fov": 1.3491744678877828
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2450026876562923,
+          "pitch": 0.08388075686559304,
+          "rotation": 0,
+          "target": "4-isseta-bar"
+        },
+        {
+          "yaw": -1.9814476843575815,
+          "pitch": 0.22785855064461558,
+          "rotation": 0,
+          "target": "0-information"
+        },
+        {
+          "yaw": -2.2252371186277156,
+          "pitch": 0.1359614523151773,
+          "rotation": 0,
+          "target": "2-drive-in-gallery-2"
+        },
+        {
+          "yaw": 1.5037578402494773,
+          "pitch": 0.12284549989112215,
+          "rotation": 0,
+          "target": "6-outdoor-gate-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-isseta-bar",
+      "name": "Isetta Bar & Waiting Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9223401456457765,
+          "pitch": 0.09048403150109507,
+          "rotation": 0,
+          "target": "5-lifestyle--sa"
+        },
+        {
+          "yaw": 0.4266222002286959,
+          "pitch": 0.00294398466629886,
+          "rotation": 0,
+          "target": "2-drive-in-gallery-2"
+        },
+        {
+          "yaw": -0.3616970412954963,
+          "pitch": 0.01434145725681546,
+          "rotation": 0,
+          "target": "3-drive-in-gallery"
+        },
+        {
+          "yaw": 1.1570995012703946,
+          "pitch": -0.010328511221340264,
+          "rotation": 0,
+          "target": "8-reception-at-the-car"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-lifestyle--sa",
+      "name": "Lifestyle & Service Advisor Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.5469585044429408,
+        "pitch": -0.02732127911129112,
+        "fov": 1.3491744678877828
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7909653763762297,
+          "pitch": 0.0923184660146319,
+          "rotation": 0,
+          "target": "4-isseta-bar"
+        },
+        {
+          "yaw": 0.6215633257479869,
+          "pitch": 0.16005238939909106,
+          "rotation": 0,
+          "target": "2-drive-in-gallery-2"
+        },
+        {
+          "yaw": 1.7951007026570762,
+          "pitch": 0.07739079459806497,
+          "rotation": 0,
+          "target": "8-reception-at-the-car"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-outdoor-gate-2",
+      "name": "Entrance Gate",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.03561896432641376,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "0-information"
+        },
+        {
+          "yaw": -1.6083132344231181,
+          "pitch": 0.06505626436130996,
+          "rotation": 0,
+          "target": "7-outdoor-gate"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-outdoor-gate",
+      "name": "Outdoor Gate",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8188981151851493,
+          "pitch": 0.0854146870000605,
+          "rotation": 0,
+          "target": "8-reception-at-the-car"
+        },
+        {
+          "yaw": -1.0860553343697958,
+          "pitch": 0.0419178186554916,
+          "rotation": 0,
+          "target": "1-diagnose-room"
+        },
+        {
+          "yaw": 0.5082030261369415,
+          "pitch": 0.09241907068848398,
+          "rotation": 0,
+          "target": "6-outdoor-gate-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-reception-at-the-car",
+      "name": "Service Reception at The Car",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.014247585730565504,
+        "pitch": 0,
+        "fov": 1.3491744678877828
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7862940272111558,
+          "pitch": -0.3059054987357399,
+          "rotation": 0,
+          "target": "1-diagnose-room"
+        },
+        {
+          "yaw": 0.8396460889149768,
+          "pitch": 0.09582958151275633,
+          "rotation": 0,
+          "target": "2-drive-in-gallery-2"
+        },
+        {
+          "yaw": 1.7420370054557317,
+          "pitch": 0.06451959537326246,
+          "rotation": 0,
+          "target": "7-outdoor-gate"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-service-room",
+      "name": "Workshop",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.03028382237392435,
+        "pitch": -0.002483752646480042,
+        "fov": 1.3491744678877828
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5762214236864347,
+          "pitch": 0.10858508502925446,
+          "rotation": 0,
+          "target": "1-diagnose-room"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "BMWBINTARO",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
